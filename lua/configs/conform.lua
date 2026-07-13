bbb2b814ca35b2c2a9ff99e266807ad74ad7bfbf
groma19 <roman.ghughunishvili@gmail.com>
2026-07-13 09:08:29 +0400

    minor UI changes and refactoring.

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -9,12 +9,6 @@ local options = {
     css = { "prettierd" },
     html = { "prettierd" },
   },
-
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
 }
 
 return options
